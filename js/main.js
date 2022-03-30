500d1900cf7bc2a00fff1c039fd35f7589e5fa2a
hy7721 <hykelly7721@gmail.com>
2022-03-30 18:47:46 +0900

    🩹 Simple fix for a non-critical issue

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {
-  // accordion
-  $('#accordion').accordion()
-
   // back to top button
   let scrollTop = $('.btn-top')
 
@@ -42,6 +39,12 @@ $(document).ready(function () {
     )
   })
 
+  $('.notice-title').click(function () {
+    $(this).next('.notice-detail').stop().slideToggle(300)
+    $(this).toggleClass('on').siblings().removeClass('on')
+    $(this).next('.notice-detail').siblings('.notice-detail').slideUp(300) // 1개씩 펼치기
+  })
+
   // layer popup
   function layerPopupOpen($t) {
     $('.play-popup' + '.' + $t)
@@ -64,4 +67,54 @@ $(document).ready(function () {
   // MO height=100vh 하단 가려지는 현상 해결
   let vh = window.innerHeight * 0.01
   document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+  // parallax effect
+  function parallaxEffect() {
+    var $fwindow = $(window)
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop
+
+    var $contents = []
+    var $backgrounds = []
+
+    $('[data-type="content"]').each(function (index, e) {
+      var $contentObj = $(this)
+
+      $contentObj.__speed = $contentObj.data('speed') || 1
+      $contentObj.__fgOffset = $contentObj.offset().top
+      $contents.push($contentObj)
+    })
+
+    $('[data-type="background"]').each(function () {
+      var $backgroundObj = $(this)
+
+      $backgroundObj.__speed = $backgroundObj.data('speed') || 1
+      $backgroundObj.__fgOffset = $backgroundObj.offset().top
+      $backgrounds.push($backgroundObj)
+    })
+
+    $fwindow.on('scroll resize', function () {
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop
+
+      $contents.forEach(function ($contentObj) {
+        var yPos = $contentObj.__fgOffset - scrollTop / $contentObj.__speed
+
+        $contentObj.css('top', yPos)
+      })
+
+      $backgrounds.forEach(function ($backgroundObj) {
+        var yPos = -(
+          (scrollTop - $backgroundObj.__fgOffset) /
+          $backgroundObj.__speed
+        )
+
+        $backgroundObj.css({
+          backgroundPosition: '50% ' + yPos + 'px',
+        })
+      })
+    })
+
+    $fwindow.trigger('scroll')
+  }
+
+  parallaxEffect()
 })
